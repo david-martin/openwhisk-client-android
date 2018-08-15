6a6e60d66a7feec633966e40ed3325fb48453fe7
David Martin <david.martin@redhat.com>
2018-08-15 04:21:42 +0100

    🎉 Initial commit

---
+++ openwhisk/src/main/java/com/example/openwhisk/OpenWhiskClient.java
@@ -0,0 +1,4 @@
+package com.example.openwhisk;
+
+public class OpenWhiskClient {
+}
---
+++ openwhiskclient/src/main/java/com/example/openwhiskclient/OpenWhiskAction.java
@@ -0,0 +1,5 @@
+package com.example.openwhiskclient;
+
+public interface OpenWhiskAction {
+    String getName();
+}
